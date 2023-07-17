794691b912876e3c4ffe0a01ffbf03e81d854c8c
mohammed hassan <mh2900001@gmail.com>
2023-07-17 06:22:45 +0300

    MUI

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,13 +5,33 @@ import { Close } from '@mui/icons-material';
 const Home = () => {
     return (
         <Box>
-            <Paper>
-                <Typography variant="h6">GYM</Typography>
-                <Typography variant="h6">$100</Typography>
-                <IconButton aria-label="Close Icon">
-                <Close/>
+            <Paper sx={{
+                position: "relative",
+                display: "flex",
+                justifyContent: "space-between",
+                width: "366px",
+                mt: "55px",
+                pt: "27px",
+                pb: "8px"
+            }}>
+                <Typography sx={{
+                    ml: "16px",
+                    fontSize: "1.3em"
+                }} variant="h6">GYM</Typography>
+                <Typography sx={{
+                    mr: "33px",
+                    fontSize: "1.4em",
+                    opacity: "0.8"
+                }} variant="h6">$100</Typography>
+                <IconButton sx={{
+                    position: "absolute",
+                    top: "0",
+                    right: "0"
+                }} aria-label="Close Icon">
+                    <Close sx={{
+                        fontSize: "20px",
+                    }} />
                 </IconButton>
-
             </Paper>
         </Box>
     );
